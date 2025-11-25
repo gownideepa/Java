2dcea3b0563c97115f2694c2848a04a60e719888
Gowni Deepa <gownideepagaaru@gmail.com>
2025-11-25 20:23:26 +0530

    Add files via upload

---
+++ Print the Output.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String person=sc.nextLine();
+        System.out.println(person);
+        sc.close();
+    }
+}
