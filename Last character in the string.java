d3cb625d3b81d8af0d89cbc5c681149aad25c336
Gowni Deepa <gownideepagaaru@gmail.com>
2025-11-25 20:27:31 +0530

    Add files via upload

---
+++ Last character in the string.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        System.out.println(a.charAt(a.length()-1));
+    }
+}
