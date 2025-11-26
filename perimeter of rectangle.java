174612d27550181b4476875525345d03a1001a7f
Gowni Deepa <gownideepagaaru@gmail.com>
2025-11-26 21:41:32 +0530

    Add files via upload

---
+++ perimeter of rectangle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=2*(a+b);
+        System.out.println(c);
+    }
+}
