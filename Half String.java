d66827cc4c9a9dd28105da03953c02329766b4c7
Gowni Deepa <gownideepagaaru@gmail.com>
2025-11-26 20:03:09 +0530

    Add files via upload

---
+++ Half String.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String w=sc.nextLine();
+        int a=(w.length())/2;
+        System.out.println(w.substring(0,a));
+       
+       
+    }
+}
