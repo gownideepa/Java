6a7357aeeaed69ca18f280b8fa524a30fbdc5a6c
Gowni Deepa <gownideepagaaru@gmail.com>
2025-11-26 21:40:55 +0530

    Add files via upload

---
+++ Kilometer to meter.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        float a=sc.nextFloat();
+        float r=a*1000;
+        int f=(int) r;
+        System.out.println(f);
+    }
+}
