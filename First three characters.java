6329c51c543eaec1af468a4334d334f07e5a39cd
Gowni Deepa <gownideepagaaru@gmail.com>
2025-11-26 21:42:39 +0530

    Add files via upload

---
+++ First three characters.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String a=s.substring(0,3);
+        System.out.println(a);
+    }
+}
