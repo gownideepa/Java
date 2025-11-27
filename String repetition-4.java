e4edb71333e1a72a1d31078e87952f638149344d
Gowni Deepa <gownideepagaaru@gmail.com>
2025-11-27 20:10:05 +0530

    Add files via upload

---
+++ String repetition-4.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+       
+        // Write your code here
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int b=sc.nextInt();
+        String res="";
+        for(int i=0;i<b;i++){
+            res+=s;
+            if(i<b-1){
+                res+=" ";
+               
+            }
+        }
+        System.out.println(res);
+    }
+}
