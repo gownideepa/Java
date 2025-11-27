ae252d51bf8a453a952fe4dedce18bb52cc8bf07
Gowni Deepa <gownideepagaaru@gmail.com>
2025-11-27 20:09:10 +0530

    Initial commit

---
+++ Half String-2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        String b=a.substring(a.length()/2);
+        System.out.println(b);
+        // Write your code here
+    }
+}
