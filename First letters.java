a47c77b59310686f4cfc77a5f9b0e7f55679bb27
Gowni Deepa <gownideepagaaru@gmail.com>
2025-11-27 20:08:39 +0530

    initial commit

---
+++ First letters.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        //Write your code here
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        String b=sc.nextLine();
+        String c=sc.nextLine();
+        System.out.print(a.charAt(0));
+        System.out.print(b.charAt(0));
+        System.out.print(c.charAt(0));
+    }
+}
