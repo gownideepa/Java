ddc82f028d716c6f4737a3cf9b045fb8b5edade1
Gowni Deepa <gownideepagaaru@gmail.com>
2025-11-27 20:10:21 +0530

    Add files via upload

---
+++ Women population.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        // Write your code here
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=a*48/100;
+        System.out.println(b);
+    }
+}
