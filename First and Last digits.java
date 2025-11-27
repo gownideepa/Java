12e885b45811d11654ed990cf34ea81761895e8d
Gowni Deepa <gownideepagaaru@gmail.com>
2025-11-27 20:08:15 +0530

    initial commit

---
+++ First and Last digits.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        String s=String.valueOf(a);
+        System.out.println(s.charAt(0));
+        System.out.println(s.charAt(s.length()-1));
+        // Write your code here
+    }
+}
