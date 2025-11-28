c11de60e4ccb8e379adc27e29bed148c318e4780
Gowni Deepa <gownideepagaaru@gmail.com>
2025-11-28 19:12:10 +0530

    Add files via upload

---
+++ Eligibility Criteria3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int m=sc.nextInt();
+        int p=sc.nextInt();
+        int c=sc.nextInt();
+        if(((m>=35)&&(p>=35)&&(c>=35))&&(((m+p)>=90)||((p+c)>=90)||((m+c)>=90))){
+            System.out.println("True");
+           
+        }
+        else{
+            System.out.println("False");
+        }
+        // Write your code here
+    }
+}
