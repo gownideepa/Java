2a3134764b172688c8eef86a6bb185695cab33a9
Gowni Deepa <gownideepagaaru@gmail.com>
2025-11-28 19:11:50 +0530

    Add files via upload

---
+++ Eligibility Criteria2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int m=sc.nextInt();
+        int p=sc.nextInt();
+        int c=sc.nextInt();
+        if((((m+p)>=100)||((p+c)>=100)||((m+c)>=100))&&((m+p+c)>=180)){
+            System.out.println("True");
+        }
+        else{
+            System.out.println("False");
+        }
+        // Write your code here
+    }
+}
