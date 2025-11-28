6c3d11721eba99ba5c17333598a873aaa3913f99
Gowni Deepa <gownideepagaaru@gmail.com>
2025-11-28 19:15:56 +0530

    Add files via upload

---
+++ Vote eligibility.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a>=18){
+            System.out.println("Eligible");
+        }
+        else{
+            System.out.println("Not Eligible");
+        }
+    }
+}
