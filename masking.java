446b4a3459ff9f87c27d9005d2022c9910e263b2
Gowni Deepa <gownideepagaaru@gmail.com>
2025-11-28 19:10:25 +0530

    Add files via upload

---
+++ masking.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        for(int i=0;i<s.length();i++){
+            if((i==0)||(i==s.length()-1)){
+                System.out.print(s.charAt(i));
+            }
+            else{
+                System.out.print("*");
+            }
+        }
+    }
+}
