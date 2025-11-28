3e52c1fefc9e27cb13713703950edb9ededaa1a8
Gowni Deepa <gownideepagaaru@gmail.com>
2025-11-28 19:13:19 +0530

    Add files via upload

---
+++ exam result.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a>50){
+            System.out.println("PASS");
+        }
+        else{
+            System.out.println("FAIL");
+        }
+    }
+}
