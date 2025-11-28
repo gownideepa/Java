3670097de31ed2312043a2d6f5dccddfd4234326
Gowni Deepa <gownideepagaaru@gmail.com>
2025-11-28 19:10:00 +0530

    Add files via upload

---
+++ Validate password.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        if(a.length()>=7){
+            System.out.println("True");
+       
+        }
+        else{
+            System.out.println("False");
+        }
+    }
+}
