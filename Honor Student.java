e15ad851a6d0e2eb73164c8c072bd0aff7145cb4
Gowni Deepa <gownideepagaaru@gmail.com>
2025-11-29 07:47:55 +0530

    Add files via upload

---
+++ Honor Student.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a<10){
+            System.out.println("HONOR STUDENT");
+        }
+        else{
+            System.out.println("NORMAL STUDENT");
+        }
+    }
+}
