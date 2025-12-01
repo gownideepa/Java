10d8ea1f2d4ac0e2411ce73ce9f00648a4b9ce3a
Gowni Deepa <gownideepagaaru@gmail.com>
2025-12-01 18:13:49 +0530

    Add files via upload

---
+++ Profit or loss.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) { 
+        Scanner sc=new Scanner(System.in);
+        int cp=sc.nextInt();
+        int sp=sc.nextInt();
+        if(sp>cp){
+            System.out.println("Profit");
+        }
+        else if(sp<cp){
+            System.out.println("Loss");
+        }
+        else{
+            System.out.println("No Profit - No Loss");
+        }
+        // Write your code here
+    }
+}
