ebde6bf65abb1a4bb0c70cca1bada56ce599b0d7
Gowni Deepa <gownideepagaaru@gmail.com>
2025-12-01 18:13:16 +0530

    Add files via upload

---
+++ Maximum number of handshakes.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=a*(a-1)/2;
+        System.out.println(b);
+    }
+}
