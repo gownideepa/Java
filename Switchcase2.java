2b361f2c2e6d102135c74cf52a06c4e17b700cdf
Gowni Deepa <gownideepagaaru@gmail.com>
2025-12-02 21:41:52 +0530

    Add files via upload

---
+++ Switchcase2.java
@@ -0,0 +1,46 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        switch(s){
+            case "January":
+                System.out.println("31");
+                break;
+            case "February":
+                System.out.println("28");
+                break;
+            case "March":
+                System.out.println("31");
+                break;
+            case "April":
+                System.out.println("30");
+                break;
+            case "May":
+                System.out.println("31");
+                break;
+            case "June":
+                System.out.println("30");
+                break;
+            case "July":
+                System.out.println("31");
+                break;
+            case "August":
+                System.out.println("31");
+                break;
+            case "September":
+                System.out.println("30");
+                break;
+            case "October":
+                System.out.println("31");
+                break;
+            case "November":
+                System.out.println("30");
+                break;
+            case "December":
+                System.out.println("31");
+                break;
+        }
+        // Write your code here
+    }
+}
