fd7f26668d9385ceb69c1eb53c54e35eb226a2e5
Gowni Deepa <gownideepagaaru@gmail.com>
2025-12-02 21:42:45 +0530

    Add files via upload

---
+++ Switchcase4.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) { 
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        switch(a){
+            case 3:
+                System.out.println("Triangle");
+                break;
+            case 4:
+                System.out.println("Quadrilateral");
+                break;
+            case 5:
+                System.out.println("Pentagon");
+                break;
+            case 6:
+                System.out.println("Hexagon");
+                break;
+            default:
+                System.out.println("Polygon");
+                break;
+        }
+        // Write your code here
+    }
+}
