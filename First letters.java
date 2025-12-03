6b8751f4b6bec991d9454565618bfa00f1d1752b
Gowni Deepa <gownideepagaaru@gmail.com>
2025-12-03 18:55:58 +0530

    Update First letters.java

--- First letters.java
+++ First letters.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Main {
     public static void main(String[] args) {
-        //Write your code here
+        
         Scanner sc=new Scanner(System.in);
         String a=sc.nextLine();
         String b=sc.nextLine();
