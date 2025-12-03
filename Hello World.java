bab20d50c5798c0f933a859af6cc3d19bcb8defd
Gowni Deepa <gownideepagaaru@gmail.com>
2025-12-03 19:01:28 +0530

    Update Hello World.java

--- Hello World.java
+++ Hello World.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
-        // Write your code here
+    
         Scanner sc=new Scanner(System.in);
         String word=sc.nextLine();
         System.out.println("Hello "+word);
