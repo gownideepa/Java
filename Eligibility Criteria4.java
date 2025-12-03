62355fd2c773c0680d7727de637c00e1393b89d8
Gowni Deepa <gownideepagaaru@gmail.com>
2025-12-03 18:54:20 +0530

    Update Eligibility Criteria4.java

--- Eligibility Criteria4.java
+++ Eligibility Criteria4.java
@@ -1,7 +1,6 @@
 import java.util.*;
 class Main {
     public static void main(String[] args) {
-        // Write your code here
         Scanner sc=new Scanner(System.in);
         int m=sc.nextInt();
         int p=sc.nextInt();
