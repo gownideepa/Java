72de4a6831526b5d27b9e22a6aac13b144bd6c2f
Gowni Deepa <gownideepagaaru@gmail.com>
2025-12-07 20:03:52 +0530

    Add files via upload

---
+++ Half pyramid3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int p=sc.nextInt();
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(p+" ");
+                p++;
+            }
+            System.out.println();
+        }
+    }
+}
