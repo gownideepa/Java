abadd524082f0be6b1d49506a9519813ad05da8e
Gowni Deepa <gownideepagaaru@gmail.com>
2025-12-07 20:00:52 +0530

    Add files via upload

---
+++ Prime number from 1 to N.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=2;i<=n;i++){
+            boolean isprime=true;
+            for(int j=2;j*j<=i;j++){
+                if(i%j==0){
+                    isprime=false;
+                    break;
+                }
+                    
+                }
+            if (isprime){
+                System.out.println(i);
+            }
+                
+            }
+                
+            
+        
+    }
+}
