9d697e812b0915c118224394b9389af6506a15b6
Gowni Deepa <gownideepagaaru@gmail.com>
2025-12-08 21:49:06 +0530

    Add files via upload

--- Solid Rectangle.java
+++ Solid Rectangle.java
@@ -4,11 +4,12 @@ class main{
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
         int b=sc.nextInt();
-        for(int i=1;i<=a;i++){
-            for(int j=1;j<=b;j++){
-                System.out.print("* ");
+        for(int i=0;i<a;i++){
+            for(int j=0;j<b;j++){
+                System.out.print("+ ");
             }
             System.out.println();
         }
-    }
+        }
+    
 }
